fa25b868d22097c6618d8848dcb99dbd9775773f
Deepak pandey <dpandey611@gmail.com>
2018-01-08 20:42:48 +0530

    first commit

---
+++ contact.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>MARKETING PAGE</title>
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="style.css">
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- //custom-theme files-->
+
+<!-- font-awesome-icons -->
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<!-- //font-awesome-icons -->
+<!-- googlefonts -->
+<link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext,vietnamese" rel="stylesheet">
+</head>
+<body>
+<header>
+<nav>
+<h1>MARKETING PAGE</h1>
+<ul id="nav">
+<li><a class="homered" href="home1.html">HOME</a></li>
+<li><a class="homeblack" href="portfolio.html">PORTFOLIO</a></li>
+<li><a class="homeblack" href="about1.html">ABOUT US</a></li>
+<li><a class="homeblack" href="contact.php">CONTACT</a></li>
+</ul>
+</nav>
+</header>
+<section class="contact-w3ls">
+	<div class="container">
+		<div class="wthree-heading">
+			<h2 class="w3l-titles">Contact Us</h2>
+		</div>
+		<div class="con-top">
+		<div class="col-lg-6 col-md-6 col-sm-6 contact-w3-agile2" data-aos="flip-left">
+			<div class="contact-agileits">
+				<h4>Get In Touch</h4>
+				<form action="#" method="post">
+					<div class="control-group form-group">
+                        <div class="controls">
+                            <label class="contact-p1">Full Name:</label> 
+                            <input type="text" class="form-control" name="name" id="name" Placeholder=" " required="">
+                            <p class="help-block"></p>
+                        </div>
+                    </div>	
+                    <div class="control-group form-group">
+                        <div class="controls">
+                            <label class="contact-p1">Phone Number:</label>
+                            <input type="tel" class="form-control" name="phone" id="phone" Placeholder=" " required="">
+							<p class="help-block"></p>
+						</div>
+                    </div>
+                    <div class="control-group form-group">
+                        <div class="controls">
+                            <label class="contact-p1">Email Address:</label>
+                            <input type="email" class="form-control" name="email" id="email" Placeholder=" " required="">
+							<p class="help-block"></p>
+						</div>
+                    </div>
+                    <div id="success"></div>
+                    <!-- For success/fail messages -->
+                    <button type="submit" class="btn btn-primary">Send</button>	
+				</form>            
+			</div>
+		</div>
+		<div class="col-lg-6 col-md-6 col-sm-6 contact-w3-agile1" data-aos="flip-right">
+			<h4 class="w3l-contact">Connect With Us</h4>
+			<p class="contact-agile1"><strong>Phone :</strong> +91 800 233 0780</p>
+			<p class="contact-agile1"><strong>Email :</strong> <a href="mailto:dpandey611@gmail.com">dpandey611@gmail.com</a></p>
+			<p class="contact-agile1"><strong>Address :</strong> BSF STS ROAD,KATTIGENHALLI YELAHANKA,BANGALORE</p>											
+			
+							<ul class="agileits_social_list">
+								<li><a href="https://www.facebook.com/pandey.deepx" class="w3_agile_facebook"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+								<li><a href="#" class="agile_twitter"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+								<li><a href="https://www.instagram.com/pandey.deepx" class="w3_agile_dribble"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+							</ul>
+						
+		</div>
+		<div class="clearfix"></div>
+		</div>
+	</div>
+</section>
+</body>
+</html>
